0bf44880a837bf35415b43b4b30fe3541f0bc90a
lSomnusl <zSomnusX@outlook.com>
2023-09-12 09:59:26 -0300

    Movimiento Listo
    Falta agregar la acción de Saltar

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,9 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+   
+    //Events
+    public static event System.Action<Vector2>OnPlayerMovement;
+
+    [SerializeField] private PlayerInput playerinput;
+    
+    
+
+    private void HandleInput(InputAction.CallbackContext context)
+    {
+        string action = context.action.name;
+
+        switch (action)
+        {
+            case "WASD":
+                Vector2 input = context.ReadValue<Vector2>();
+                OnPlayerMovement?.Invoke(input);
+                break;
+        }
+    }
+
+
+
+    private void OnEnable()
+    {
+        playerinput.onActionTriggered += HandleInput;
+    }
+
+    private void OnDisable()
+    {
+        playerinput.onActionTriggered -= HandleInput;
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+   
+    private Vector2 input;
+    private Rigidbody2D rb;
+    public float speed;
+
+    
+    private void Move(Vector2 input)
+    {
+        this.input = input;
+    }
+
+    private void OnEnable()
+    {
+        InputManager.OnPlayerMovement += Move;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.OnPlayerMovement -= Move;
+    }
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody2D>();    
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 velocity = new Vector2 (input.x,0);
+        rb.position += velocity * speed * Time.fixedDeltaTime;
     }
 }
